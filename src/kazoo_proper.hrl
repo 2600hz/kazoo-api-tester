da75ca3f883f6291a96bf40c451f9d2c4aeb6b75
James Aimonetti <james.aimonetti@gmail.com>
2019-01-12 00:51:22 +0000

    initial migration of core/kazoo_proper

---
+++ src/kazoo_proper.hrl
@@ -0,0 +1,32 @@
+-ifndef(KAZOO_PROPER_HRL).
+-include_lib("kazoo_stdlib/include/kz_types.hrl").
+-include_lib("kazoo_stdlib/include/kz_databases.hrl").
+
+-define(FAILED_RESPONSE, <<"{}">>).
+
+-define(DEBUG(Fmt)
+       ,_ = data:debug(pqc_log:log_info(), Fmt)
+       ).
+
+-define(DEBUG(Fmt, Args)
+       ,_ = data:debug(pqc_log:log_info(), Fmt, Args)
+       ).
+
+-define(INFO(Fmt)
+       ,_ = data:info(pqc_log:log_info(), Fmt)
+       ).
+
+-define(INFO(Fmt, Args)
+       ,_ = data:info(pqc_log:log_info(), Fmt, Args)
+       ).
+
+-define(ERROR(Fmt)
+       ,_ = data:error(pqc_log:log_info(), Fmt)
+       ).
+
+-define(ERROR(Fmt, Args)
+       ,_ = data:error(pqc_log:log_info(), Fmt, Args)
+       ).
+
+-define(KAZOO_PROPER_HRL, 'true').
+-endif.
